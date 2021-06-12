2e2866693054c8d7ca99bc8fc231c2f7e15bfffc
JonnyTran <JonnyTran@github.com>
2021-06-12 02:06:09 -0500

    all working

--- conv.py
+++ conv.py
@@ -1,4 +1,6 @@
 import copy
+import random
+from typing import Union
 
 import numpy as np
 import pandas as pd
@@ -666,3 +668,25 @@ def adamic_adar(indexA, valueA, indexB, valueB, m, k, n, coalesced=False, sampli
         row, col, values = row[idx], col[idx], values[idx]
 
     return torch.stack([row, col], dim=0), values
+
+
+def negative_sample(edge_index, M: int, N: int, n_sample_per_edge: int):
+    num_neg_samples = edge_index.size(1) * n_sample_per_edge
+    num_neg_samples = int(min(num_neg_samples, M * N - edge_index.size(1)))
+    rng = range(M * N)
+    idx = (edge_index[0] * N + edge_index[1]).to('cpu')  # idx = N * i + j
+
+    perm = torch.tensor(random.sample(rng, num_neg_samples))
+    mask = torch.from_numpy(np.isin(perm, idx)).to(torch.bool)
+    rest = mask.nonzero().view(-1)
+    while rest.numel() > 0:  # pragma: no cover
+        tmp = torch.tensor(random.sample(rng, rest.size(0)))
+        mask = torch.from_numpy(np.isin(tmp, idx)).to(torch.bool)
+        perm[rest] = tmp
+        rest = rest[mask.nonzero().view(-1)]
+
+    row = perm // N
+    col = perm % N
+    neg_edge_index = torch.stack([row, col], dim=0).long()
+
+    return neg_edge_index.to(edge_index.device)
